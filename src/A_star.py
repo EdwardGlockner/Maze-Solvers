8bec33faf28a5abe92d53c1a0b66a3e4d74f8b97
EdwardGlockner <edward.glockner5@gmail.com>
2023-02-12 17:42:04 +0100

    Create files

---
+++ src/A_star.py
@@ -0,0 +1,12 @@
+import sys
+import os
+sys.path.append(str(sys.path[0][:-14]))
+
+dirname = os.getcwd()
+dirname = dirname.replace("/src", "")
+
+
+class A_star():
+    def __init__(self):
+        pass
+
