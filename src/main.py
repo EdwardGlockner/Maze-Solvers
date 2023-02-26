4271706bbfd6517aafba9833722a114ac3975c0d
EdwardGlockner <edward.glockner5@gmail.com>
2023-02-26 11:08:12 +0100

    New files

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ sys.path.insert(1, os.path.join(dirname))
 
 from MazeGen import MazeGen
 
-Maze = MazeGen(width=15, height=15)
+Maze = MazeGen(width=15, height=18)
 hasVisited = [(1,1)]
 Maze.visit(1,1, hasVisited)
 Maze.print_maze()
