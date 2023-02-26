ba7c1a2fd9a0828eb11ff5f80c44a3534098ceb2
EdwardGlockner <edward.glockner5@gmail.com>
2023-02-26 13:15:33 +0100

    Pygame

---
+++ src/Graphics.py
@@ -0,0 +1,20 @@
+import pygame
+import os
+import time
+import sys
+
+
+class Graphics():
+    def __init__(self, width = 800, height = 800):
+        pass
+
+
+    def redraw_window(self):
+        pass
+
+    
+    def live(self):
+        run = True
+        while run:
+            run = False
+
--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ sys.path.insert(1, os.path.join(dirname))
 
 from MazeGen import MazeGen
 
-Maze = MazeGen(width=15, height=18)
+Maze = MazeGen(width=40, height=40)
 hasVisited = [(1,1)]
 Maze.visit(1,1, hasVisited)
 Maze.print_maze()
